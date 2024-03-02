60572a07f2fdb8f14f49b6b33c254bfe98626ea3
John Aybar <jorkle0895@gmail.com>
2024-03-02 01:34:39 -0500

    - README.md   - Added basic info   - Added disclosure info for compliance with Obsidian's developer policies   - Added Attributon section - LICENSE.txt   - Added LICENSE.txt (MIT License) - main.ts, manifest.json, package.json   - Cleaned up the files (renamed stuff)
    ---

--- main.ts
+++ main.ts
@@ -1,54 +1,25 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface FabricSettings {
+	fabricAPIKey: string;
+	fabricAPIServer: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: FabricSettings = {
+fabricAPIKey: ''
+fabricAPIServer: ''
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class FabricPlugin extends Plugin {
+	settings: FabricSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
+			id: 'open-extract-wisdom-prompt-active-file',
+			name: 'open extract wisdom prompt and populate active file',
 			checkCallback: (checking: boolean) => {
 				// Conditions to check
 				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
@@ -56,7 +27,7 @@ export default class MyPlugin extends Plugin {
 					// If checking is true, we're simply "checking" if the command can be run.
 					// If checking is false, then we want to actually perform the operation.
 					if (!checking) {
-						new SampleModal(this.app).open();
+						new ExtractWisdomModal(this.app).open();
 					}
 
 					// This command will only show up in Command Palette when the check function returns true
@@ -66,16 +37,8 @@ export default class MyPlugin extends Plugin {
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new FabricSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -91,7 +54,8 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
+// this adds a Modal for the extract_wisdom Fabric pattern.
+class ExtractWisdomModal extends Modal {
 	constructor(app: App) {
 		super(app);
 	}
@@ -107,10 +71,10 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class FabricSettingTab extends PluginSettingTab {
+	plugin: FabricPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: FabricPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -120,15 +84,27 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
+		// This adds a configurable 'Fabric API Key' setting.
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Fabric API Key')
+			.setDesc('Required')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('')
+				.setValue(this.plugin.settings.fabricAPIKey)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.fabricAPIKey = value;
 					await this.plugin.saveSettings();
 				}));
+
+		// This adds a configurable 'Fabric API Server' setting.
+		new Setting(containerEl)
+			.setName('Fabric API Server')
+			.setDesc('Required')
+			.addText(text => text
+				.setPlaceholder('')
+				.setValue(this.plugin.settings.fabricAPIServer)
+				.onChange(async (value) => {
+					this.plugin.settings.fabricAPIServer = value;
+					await this.plugin.saveSettings();
 	}
 }
